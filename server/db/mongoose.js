d738ab595c45da332a8ab5ee7d1b220e385e10fc
Mihajlo Perendija <mihajlo.perendija@gmail.com>
2019-08-22 12:03:02 +0200

    mongo change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,8 @@ mongoose.set('useCreateIndex', true);
 
 mongoose.Promise = global.Promise;
 
-const uri = "mongodb+srv://mihajlo:instagram-internship@instagram-internship-nzjce.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb://mihajlo:instagraminternship@instagram-internship-shard-00-00-nzjce.mongodb.net:27017,instagram-internship-shard-00-01-nzjce.mongodb.net:27017,instagram-internship-shard-00-02-nzjce.mongodb.net:27017/test?ssl=true&replicaSet=instagram-internship-shard-0&authSource=admin&retryWrites=true&w=majority";
+
 
 
 mongoose.connect(uri, (err) => {
