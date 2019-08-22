ce8a492efc4dd8c61355777ab26562d8397b64d8
Mihajlo Perendija <mihajlo.perendija@gmail.com>
2019-08-22 11:43:35 +0200

    mongo change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,6 +8,12 @@ mongoose.Promise = global.Promise;
 const uri = "mongodb+srv://mihajlo:instagram%2Dinternship@instagram-internship-nzjce.mongodb.net/test?retryWrites=true&w=majority";
 
 
-mongoose.connect(uri);
+mongoose.connect(uri, (err) => {
+        if(err) {
+            console.log('Some problem with the connection ' +err);
+        } else {
+            console.log('The Mongoose connection is ready');
+        }
+};
 
 module.exports = {mongoose}
